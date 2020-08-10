81cce5b24084e91ac31914cfac27bf23162988a0
Leonardo Quadros <leooq.d@gmail.com>
2020-08-10 04:54:07 -0300

    add informações do estoque nos endpoints

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,17 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :price
+
+  has_many :stock_items
+
+  class StockItemSerializer < ActiveModel::Serializer
+    attributes :store_id, :store_name, :store_address, :quantity
+
+    def store_name
+      object.store.name
+    end
+
+    def store_address
+      object.store.address
+    end
+  end
 end
--- app/serializers/store_serializer.rb
+++ app/serializers/store_serializer.rb
@@ -1,3 +1,21 @@
-class ProductSerializer < ActiveModel::Serializer
+class StoreSerializer < ActiveModel::Serializer
   attributes :id, :name, :address
+
+  has_many :stock_items
+
+  class StockItemSerializer < ActiveModel::Serializer
+    attributes :product_id, :product_name, :product_description, :product_price, :quantity
+
+    def product_name
+      object.product.name
+    end
+
+    def product_description
+      object.product.description
+    end
+
+    def product_price
+      object.product.price
+    end
+  end
 end
