115d16795d5f9adb34ce94e8030be68ce792f1f9
Leonardo Quadros <leooq.d@gmail.com>
2020-08-10 00:41:53 -0300

    create controller store

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,44 @@
+class StoresController < ApplicationController
+  before_action :set_store, only: %i[show update destroy]
+
+  def show
+    render json: @store
+  end
+
+  def create
+    store = Store.new(store_params)
+
+    if store.save
+      render json: store, status: :created
+    else
+      render json: store.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @store.update(store_params)
+      render json: @store, status: :ok
+    else
+      render json: @store.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @store.destroy
+    head 204
+  end
+
+  private
+
+  def set_store
+    @store = Store.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { message: e.message }, status: :not_found
+  end
+
+  def store_params
+    params.require(:store).permit(
+      :name,
+      :address)
+  end
+end
