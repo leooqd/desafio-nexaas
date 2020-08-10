5200c2957eca75af9c42c862ba8df8bc025d0246
Leonardo Quadros <leooq.d@gmail.com>
2020-08-10 04:53:28 -0300

    add validation

--- app/models/stock_item.rb
+++ app/models/stock_item.rb
@@ -2,6 +2,8 @@ class StockItem < ApplicationRecord
   belongs_to :product
   belongs_to :store
 
+  validates :product_id, uniqueness: { scope: %i[store_id] }
+
   validates_associated :product
   validates_associated :store
 
