9ddba4274fb8430a002c463f8e3776069d475e13
Leonardo Quadros <leooq.d@gmail.com>
2020-08-10 04:22:31 -0300

    add relacionamento

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :stock_items
+  has_many :stores, through: :stock_items
+
   validates_presence_of :name
   validates_numericality_of :price, greater_than: 0
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,6 @@
 class Store < ApplicationRecord
+  has_many :stock_items
+  has_many :products, through: :stock_items
+
   validates_presence_of :name, :address
 end
