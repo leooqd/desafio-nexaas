c63eece5099916c9761dc12f4103e801b73aabdc
Leonardo Quadros <leooq.d@gmail.com>
2020-08-10 03:14:35 -0300

    add routes stock_item

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :products, except: [:index]
   resources :stores, except: [:index]
 
+  post  '/stock_items/update', to: 'stock_items#update', as: :stock_items_update
 end
